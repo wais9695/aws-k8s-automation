f508c3561ce61f15c3df5fa65946a04ea247a441
Wais Ahmadi <waisahmadi0907@gmail.com>
2026-01-12 17:02:54 +0100

    remove public ip

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "region" { default = "eu-central-1" }
 variable "instance_type" { default = "c7i-flex.large" }
 variable "key_name" { default = "EC2" }
-variable "my_ip" { default = "95.90.183.114" }
+variable "my_ip" { }
 
